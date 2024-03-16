e5682da6e7fd16ff785b79c3a94c4547104375ac
SreeNath <gootysreenathreddy@gmail.com>
2024-03-16 23:13:54 +0530

    Add files via upload

---
+++ Spark_Senario.py
@@ -0,0 +1,73 @@
+/*You are given a dataframe with columns id, email, sent_date. The email id is a frequently updated column in this dataframe. Your task is to find out latest email of the students based on the most recent update_date */
+
+
+input
++---+-----+-----------------+-------------------+
+| id| name|            email|        update_date|
++---+-----+-----------------+-------------------+
+|  1| john| john@example.com|2023-09-01 10:30:00|
+|  2| jane| jane@example.com|2023-09-02 15:45:00|
+|  1| john| john@example.org|2023-09-03 15:45:00|
+|  2| jane| jane@example.org|2023-09-04 15:45:00|
+|  3|  bob|  bob@example.com|2023-09-05 15:45:00|
+|  4|alice|alice@example.org|2023-09-06 10:30:00|
+|  5|  eve|    eve@gmail.com|2023-09-07 15:45:00|
+|  4|alice|  alice@gmail.com|2023-09-08 15:45:00|
+|  5|  eve|  eve@example.com|2023-09-09 15:45:00|
++---+-----+-----------------+-------------------+
+output
++---+-----+----------------+-------------------+
+| id| name|           email|        update_date|
++---+-----+----------------+-------------------+
+|  1| john|john@example.org|2023-09-03 15:45:00|
+|  2| jane|jane@example.org|2023-09-04 15:45:00|
+|  3|  bob| bob@example.com|2023-09-05 15:45:00|
+|  4|alice| alice@gmail.com|2023-09-08 15:45:00|
+|  5|  eve| eve@example.com|2023-09-09 15:45:00|
++---+-----+----------------+-------------------+
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql import Window
+
+# Initialize SparkSession
+spark = SparkSession.builder \
+ .appName("CreateDataFrameFromSQLTable") \
+ .getOrCreate()
+
+# Define the schema for the DataFrame
+schema = StructType([
+ StructField("id", IntegerType(), nullable=False),
+ StructField("name", StringType(), nullable=False),
+ StructField("email", StringType(), nullable=False),
+ StructField("update_date", StringType(), nullable=False)
+])
+
+# Create a list of tuples containing the data
+data = [
+ (1, 'john', 'john@example.com', '2023-09-01 10:30:00'),
+ (2, 'jane', 'jane@example.com', '2023-09-02 15:45:00'),
+ (1, 'john', 'john@example.org', '2023-09-03 15:45:00'),
+ (2, 'jane', 'jane@example.org', '2023-09-04 15:45:00'),
+ (3, 'bob', 'bob@example.com', '2023-09-05 15:45:00'),
+ (4, 'alice', 'alice@example.org', '2023-09-06 10:30:00'),
+ (5, 'eve', 'eve@gmail.com', '2023-09-07 15:45:00'),
+ (4, 'alice', 'alice@gmail.com', '2023-09-08 15:45:00'),
+ (5, 'eve', 'eve@example.com', '2023-09-09 15:45:00')
+]
+
+# Create a DataFrame from the list of tuples with the defined schema
+
+df=spark.createDataFrame(data,schema=schema)
+# Convert update_date column to date type
+df.withColumn('update_date',to_timestamp('update_date'))
+# Define window specification for dense ranking by id and ordering by update_date
+spec=Window.partitionBy("id").orderBy(col("update_date").desc())
+# Add a column for the latest email rank using dense ranking
+df=df.withColumn('laste_email_id',dense_rank().over(spec))
+# Filter the DataFrame to keep only rows with the latest email for each id and drop unnecessary column
+final=df.filter(col('laste_email_id')==1).drop('laste_email_id')
+# Show the final DataFrame
+final.show()
