c5c82d2cc1f313987b4a8084d9d0ef750c48d425
SreeNath <gootysreenathreddy@gmail.com>
2024-04-29 23:12:09 +0530

    Update pivot_function.py

--- pivot_function.py
+++ pivot_function.py
@@ -1,4 +1,4 @@
-question----
+question 1----
 id name amout
 1  A   30
 1  B   80
@@ -69,3 +69,53 @@ df1.show()
 |  1|  80| 80|
 |  2|null| 30|
 +---+----+---+
+
+question 2-----------------------------------------------------------------------------------------------------------------
+Input-
+
+name    sub      mark
+Siva   Math       80
+siva   Eng        85
+Anu    Math       85
+Anu    Eng        80
+Rahul  Math       90
+Rahul  Eng        85
+
+Output-S
+
+Name Math Eng
+Siva 80 85
+Anu  85 80
+Rahul 90 85
+
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+data=[('siva','Math',80),
+      ('siva','Eng',85),
+      ('Anu','Math',85),
+	  ('Anu','Eng',80),
+	  ('Rahul','Math',90),
+	  ('Rahul','Eng',85)]
+	  
+schema = StructType([
+       StructField("name",StringType(),True), \
+	   StructField("sub",StringType(),True), \
+	   StructField("mark",IntegerType(),True)])	 
+
+df = spark.createDataFrame(data=data,schema=schema)
+df.show()
+
+solution 1-----------------------
+from pyspark.sql.functions import first, last,sum,max
+df1=df.groupBy('name').pivot('sub').max('mark').alias('marks').orderBy('name').show()
+
+
+df.createOrReplaceTempView('df_stud')
+
+solution 2----------------------
+df2=spark.sql("""select 
+name,
+sum(case when sub='Math' then mark end) as Math,
+sum(case when sub='Eng' then mark end) as Eng
+from df_stud
+group by name """)
+df2.show()
