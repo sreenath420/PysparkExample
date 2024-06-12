eed073597b54e6a599a6cc47e5ca61600d5b7c6d
SreeNath <gootysreenathreddy@gmail.com>
2024-06-12 22:36:59 +0530

    Update daily_practice.py

--- daily_practice.py
+++ daily_practice.py
@@ -115,3 +115,76 @@ df1=spark.sql("""SELECT
 REGEXP_REPLACE(TRIM(Full_name), ' +', ' ') AS normalized_name
 FROM df_view """)
 df1.show()
+
+
+-------------------------------------------------find the topper in each semester------------------------------------------------------------------------------
+Input-
++---+----+--------+---------+-----+
+| id|name|semester|  subject|marks|
++---+----+--------+---------+-----+
+|  1|   A|       1|  PHYSICS|  100|
+|  1|   A|       2|  PHYSICS|  150|
+|  1|   A|       3|  PHYSICS|  200|
+|  1|   A|       4|  PHYSICS|  250|
+|  1|   A|       1|CHEMISTRY|   50|
+|  1|   A|       2|CHEMISTRY|  250|
+|  1|   A|       3|CHEMISTRY|  200|
+|  1|   A|       4|CHEMISTRY|  350|
+|  2|   B|       1|  PHYSICS|  150|
+|  2|   B|       2|  PHYSICS|  250|
+|  2|   B|       3|  PHYSICS|  100|
+|  2|   B|       4|  PHYSICS|  200|
+|  2|   B|       1|CHEMISTRY|  150|
+|  2|   B|       2|CHEMISTRY|  150|
+|  2|   B|       3|CHEMISTRY|  250|
+|  2|   B|       4|CHEMISTRY|  300|
++---+----+--------+---------+-----+
+ouput-
++----+--------+-----+------------+
+|name|semester|marks|marks_window|
++----+--------+-----+------------+
+|   B|       1|  300|           1|
+|   A|       2|  400|           1|
+|   B|       2|  400|           1|
+|   A|       3|  400|           1|
+|   A|       4|  600|           1|
++----+--------+-----+------------+
+
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+data =[
+(1,'A',1,'PHYSICS',100),
+(1,'A',2,'PHYSICS',150),
+(1,'A',3,'PHYSICS',200),
+(1,'A',4,'PHYSICS',250),
+(1,'A',1,'CHEMISTRY',50),
+(1,'A',2,'CHEMISTRY',250),
+(1,'A',3,'CHEMISTRY',200),
+(1,'A',4,'CHEMISTRY',350),
+(2,'B',1,'PHYSICS',150),
+(2,'B',2,'PHYSICS',250),
+(2,'B',3,'PHYSICS',100),
+(2,'B',4,'PHYSICS',200),
+(2,'B',1,'CHEMISTRY',150),
+(2,'B',2,'CHEMISTRY',150),
+(2,'B',3,'CHEMISTRY',250),
+(2,'B',4,'CHEMISTRY',300)]
+schema=StructType([
+StructField("id",IntegerType(),True),\
+StructField("name",StringType(),True),\
+StructField("semester",IntegerType(),True),\
+StructField("subject",StringType(),True),\
+StructField("marks",IntegerType(),True)
+])
+df=spark.createDataFrame(data=data,schema=schema)
+df.show()
+
+from pyspark.sql.window import Window
+from pyspark.sql.functions import dense_rank
+sem_marks=df.groupBy(df.name,df.semester)\
+  .agg(sum(df.marks).alias('marks'))\
+   .orderBy(df.semester) 
+windowSpec = Window.partitionBy(sem_marks.semester).orderBy(sem_marks.marks.desc())
+window_df=sem_marks.withColumn('marks_window',dense_rank().over(windowSpec))
+result=window_df.filter(window_df.marks_window == 1)
+result2 = result.select(result.name,result.semester,result.marks).show()
+result2.show()
