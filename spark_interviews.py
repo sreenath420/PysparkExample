82121dc7e9f1f6df2e6bbceb903500f4c8423f07
SreeNath <gootysreenathreddy@gmail.com>
2024-07-12 01:05:16 +0530

    Update spark_interviews.py

--- spark_interviews.py
+++ spark_interviews.py
@@ -388,3 +388,37 @@ employee_df.show()
 df=employee_df.withColumn('new_count',concat_ws("",substring(col('fname'),1,1),substring(col('lname'),1,1)))
 df1=df.groupBy('new_count').count()
 df1.show()
+
+
+---------------------------->convert string values to numeric in pyspark<------------------------------------------
+--->input<----
++------+
+|salary|
++------+
+|  1000|
+|   ind|
+|  2000|
+|  4000|
+|  5000|
+|   usa|
++------+
+--->output<--
+   output|
++------+------+
+|  1000|  1000|
+|   ind|     0|
+|  2000|  2000|
+|  4000|  4000|
+|  5000|  5000|
+|   usa|     0|
++------+------+
+creating a dataframe 
+data=[(1000,),('ind',),(2000,),(4000,),(5000,),('usa',)]
+schema=['salary']
+df=spark.createDataFrame(data,schema=schema)
+from pyspark.sql.functions import regexp_extract,col,when
+df_numeric="^[0-9]+$"
+#df1=df.withColumn('val',when(regexp_extract(col('salary'),df_numeric,0)=="",0)
+df = df.withColumn("output", when(regexp_extract(col("salary"), df_numeric, 0) == "", 0).otherwise(col('salary').cast('integer')))
+
+
