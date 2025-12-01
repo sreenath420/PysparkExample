9b6e10e7b01bcbe46585c0144c34852b349d6204
SreeNath <gootysreenathreddy@gmail.com>
2025-12-01 18:31:12 +0530

    Update rename_multiple_column.py

--- rename_multiple_column.py
+++ rename_multiple_column.py
@@ -1,17 +1,32 @@
 ------>how to rename mulitple columns at time in spark <-----
 
-def rename_cols(data,old_col,new_col):
-  for old_col,new_col in zip(old_col,new_col):
-    data=data.withColumnRenamed(old_col,new_col)
+# Example: Create df_book DataFrame
+data = [
+  ['Book A', 'Author X', 'Desc 1', 'Fiction', 'Pub1', '2021-01-01', 10.99],
+  ['Book B', 'Author Y', 'Desc 2', 'Non-Fiction', 'Pub2', '2022-02-02', 12.99]
+]
+columns = [
+  'Title', 'Authors', 'Description', 'Category', 'Publisher', 'Publish Date', 'Price'
+]
+df_book = spark.createDataFrame(data, columns)
+
+def rename_cols(data, old_col, new_col):
+  for old, new in zip(old_col, new_col):
+    data = data.withColumnRenamed(old, new)
   return data
 
-old_col=['Title','Authors','Description','Category','Publisher','Publish Date','Price']
-new_col=['Title1','Authors1','Description1','Category1','Publisher1','Publish_Date','Price1']
+old_col = [
+  'Title', 'Authors', 'Description', 'Category', 'Publisher', 'Publish Date', 'Price'
+]
+new_col = [
+  'Title1', 'Authors1', 'Description1', 'Category1', 'Publisher1', 'Publish_Date', 'Price1'
+]
 
-df_renamed=rename_cols(df_book,old_col,new_col)
-df_renamed.show()
+df_renamed = rename_cols(df_book, old_col, new_col)
+display(df_renamed)
 
 
-+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+
-|              Title1|            Authors1|        Description1|           Category1|          Publisher1|        Publish_Date|              Price1|
-+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+
+output:-
+Title1	Authors1	Description1	Category1	Publisher1	Publish_Date	Price1
+Book A	Author X	Desc 1	Fiction	Pub1	2021-01-01	10.99
+Book B	Author Y	Desc 2	Non-Fiction	Pub2	2022-02-02	12.99
